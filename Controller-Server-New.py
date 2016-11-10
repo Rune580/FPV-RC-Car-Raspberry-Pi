47bd7bf0199443492804e4c2d54475fe2cee082a
RuneMasterGaming <runemaster580@gmail.com>
2016-11-09 19:02:00 -0500

    Add files via upload

---
+++ Controller-Server-New.py
@@ -0,0 +1,34 @@
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+input_list = [7,11]
+output_list = [12,13,15]
+GPIO.setup(input_list, GPIO.IN)
+GPIO.setup(output_list, GPIO.OUT)
+
+def get_cont():
+    a = 0
+    while a != 4:
+        if a == 0:
+            fwd = GPIO.input(7)
+            a += 1
+        elif a == 1:
+            bwd = GPIO.input(11)
+            a += 1
+        elif a == 2:
+            fab = fwd - bwd
+            a = 1
+        elif a == 3:
+            if fab == -1:
+                #print("Backwards")
+                GPIO.output(15, GPIO.LOW)
+                GPIO.output(13, GPIO.HIGH)
+            elif fab == 1:
+                #print("Forwards")
+                GPIO.output(13, GPIO.LOW)
+                GPIO.output(15, GPIO.HIGH)
+            else:
+                #print("Idle")
+                GPIO.output(13, GPIO.LOW)
+                GPIO.output(15, GPIO.LOW)
+            a = 0
